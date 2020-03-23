f52dc15942b805a0048ae8460543e84b9558164c
michellebuser <michellebuser2@gmail.com>
2020-03-23 21:25:57 +0100

    Initial commit

---
+++ scripts/javascript.js
@@ -0,0 +1,63 @@
+setInterval(clockHands, 1);
+setInterval(changeBackground,1) //nog aanpassen
+
+
+function clockHands (){
+  var today = new Date();
+  var hour = today.getHours() %12 * 30;
+  var minute = today.getMinutes() * 6;
+  var second = today.getSeconds() * 6; 
+
+  document.getElementById("hours").style.transform="rotate("+ hour+ "deg)";
+  document.getElementById("minutes").style.transform="rotate("+ minute+ "deg)";
+  document.getElementById("seconds").style.transform="rotate("+ second+ "deg)";
+}
+
+function changeBackground(){
+  var today = new Date();
+  var hour = today.getHours();
+  var minute = today.getMinutes() ;
+  var second = today.getSeconds(); 
+  var body = document.body;
+
+  if ("0"<= second&&second < "15") {
+    body.classList.add("morning");
+    body.classList.remove("night");
+  }
+
+  else if ("15"<= second&&second < "30") {
+    body.classList.add("afternoon");
+    body.classList.remove("morning");
+  } 
+
+  else if ("30"<= second&&second < "45") {
+    body.classList.add("evening");
+    body.classList.remove("afternoon");
+  } 
+
+
+  else {
+      body.classList.add("night");
+      body.classList.remove("evening");
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
